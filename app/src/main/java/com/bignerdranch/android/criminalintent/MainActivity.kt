fcf11732d172c3d060334e3f2d611956a372c583
islandsexit <hezkydoc@gmail.com>
2022-05-25 22:20:54 +0300

    init

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.bignerdranch.android.criminalintent
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import java.util.UUID
+
+class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
+
+    lateinit var test_button: Button
+    private val crimeRepository = CrimeRepository.get()
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        test_button = findViewById(R.id.test_add) as Button
+        
+        test_button.setOnClickListener{
+            crimeRepository.updateCrime(Crime())
+        }
+
+        setContentView(R.layout.activity_main)
+        val MY_READ_EXTERNAL_REQUEST : Int = 1
+        if (checkSelfPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), MY_READ_EXTERNAL_REQUEST)
+        }
+
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = CrimeListFragment.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+
+    }
+
+    override fun onCrimeSelected(crimeId: UUID) {
+        val fragment = CrimeFragment.newInstance(crimeId)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
